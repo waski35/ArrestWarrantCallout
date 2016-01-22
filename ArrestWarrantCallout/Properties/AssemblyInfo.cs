5d8c5201111b0e9066c2509c167c035717c93043
MarcinKalat <mkalat@mkalat.pl>
2016-01-22 06:12:11 +0100

    release

--- ArrestWarrantCallout/Properties/AssemblyInfo.cs
+++ ArrestWarrantCallout/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.22.0451")]
-[assembly: AssemblyFileVersion("0.4.22.0451")]
+[assembly: AssemblyVersion("0.4.22.0511")]
+[assembly: AssemblyFileVersion("0.4.22.0511")]
