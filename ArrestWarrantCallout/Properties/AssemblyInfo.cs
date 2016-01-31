7bae21bcc5def57e012fc07d47f9c70ed30203db
MarcinKałat <marcin.kalat@hotmail.com>
2016-01-31 19:37:11 +0100

    relinked with rph 0.36

--- ArrestWarrantCallout/Properties/AssemblyInfo.cs
+++ ArrestWarrantCallout/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.24.0339")]
-[assembly: AssemblyFileVersion("0.4.24.0339")]
+[assembly: AssemblyVersion("0.4.31.1836")]
+[assembly: AssemblyFileVersion("0.4.31.1836")]
